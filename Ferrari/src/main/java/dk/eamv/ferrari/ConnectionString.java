5bdd2f0a8c5b37cbd363761cdefc251e7d8bfcc5
Christiansgh <workmailchristian@gmail.com>
2023-05-02 23:02:05 +0200

    Made connectionstring class & implemented method in database.

---
+++ Ferrari/src/main/java/dk/eamv/ferrari/ConnectionString.java
@@ -0,0 +1,9 @@
+package dk.eamv.ferrari;
+
+public class ConnectionString {
+    private static String connectionString = "";
+
+    public static String getConnectionString() {
+        return connectionString;
+    }
+}
