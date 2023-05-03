c31602e0564bbb2d4a2f8efa0ae92f0cd9fd5891
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-03 22:29:10 +0200

    Added WIP login authentication

--- Ferrari/src/main/java/dk/eamv/ferrari/login/LoginController.java
+++ Ferrari/src/main/java/dk/eamv/ferrari/login/LoginController.java
@@ -1,5 +1,9 @@
 package dk.eamv.ferrari.login;
 
+import dk.eamv.ferrari.employee.Employee;
+import dk.eamv.ferrari.employee.EmployeeModel;
+import dk.eamv.ferrari.login.LoginView;;
+
 /**
  * Lavet af: Christian
  * Tjekket af:
@@ -7,6 +11,10 @@ package dk.eamv.ferrari.login;
  */
 public class LoginController {
     public static void authenticate() {
+        Employee employee = EmployeeModel.authenticate(LoginView.getUsernameInput(), LoginView.getPasswordInput());
+        if (employee != null) {
+
+        }
         //Take the input from textfield & passwordfield. Use it in LoginModels login query.
         //if returned rows == 1 -> get enum(byte) containing status being seller or saleschief.
         //else displayLoginFailed(); 
