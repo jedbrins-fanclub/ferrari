6f58071d7ac494082f703ec5cdc506e5fc3ccb5a
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-03 23:44:48 +0200

    SceneManager fixes and changes - Simplified SceneManager by removing root and scene properties. - We now call the function SceneManager.changeScene() instead of setScene(). - Fixed stylesheet addition for root element. - Fixed scene being null if not created beforehand.

--- Ferrari/src/main/java/dk/eamv/ferrari/scenemanager/SceneManager.java
+++ Ferrari/src/main/java/dk/eamv/ferrari/scenemanager/SceneManager.java
@@ -1,34 +1,31 @@
 package dk.eamv.ferrari.scenemanager;
 
-import java.lang.Object.*;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
 import java.util.Objects;
-import dk.eamv.ferrari.sidebar.SidebarController;
-import dk.eamv.ferrari.sidebar.SidebarView;
+import dk.eamv.ferrari.login.LoginView;
 
 public class SceneManager {
-    private static Parent root = new BorderPane();
     private static Stage stage;
-    private static Scene scene;
 
-    public static void init(Stage stage) {
-        root.getStylesheets().add(Objects.requireNonNull(SceneManager.class.getResource("/style.css")).toExternalForm());
-
-        scene = new Scene(root);
-
-        SceneManager.stage = stage;
+    public static void init(Stage initialStage) {
+        stage = initialStage;
+        changeScene(LoginView.getScene());
         stage.setMaximized(true);
-        stage.setScene(scene);
         stage.setTitle("Ferrari");
         stage.getIcons().add(new Image("file:src/main/resources/media/ferrari-emoji.png"));
         stage.show();
     }
 
     public static void changeScene(Parent root) {
-        stage.getScene().setRoot(root);
+        root.getStylesheets().add(Objects.requireNonNull(SceneManager.class.getResource("/style.css")).toExternalForm());
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            stage.setScene(new Scene(root));
+        } else {
+            stage.getScene().setRoot(root);
+        }
     }
 }
