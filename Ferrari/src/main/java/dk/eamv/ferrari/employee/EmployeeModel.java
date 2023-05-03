c81860c67ace1b51fba834b4dfa8f3c30fc13bd4
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-03 15:54:28 +0200

    Rename SellerData -> EmployeeModel.java

--- Ferrari/src/main/java/dk/eamv/ferrari/employee/EmployeeModel.java
+++ Ferrari/src/main/java/dk/eamv/ferrari/employee/EmployeeModel.java
@@ -1,19 +1,18 @@
-package dk.eamv.ferrari.database;
+package dk.eamv.ferrari.employee;
 
 import dk.eamv.ferrari.database.Database;
-import dk.eamv.ferrari.seller.Seller;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class SellerData {
-    public static Seller getSellerFromID(int id) {
+public class EmployeeModel {
+    public static Employee getSellerFromID(int id) {
         ResultSet rs = Database.query("SELECT * FROM dbo.Employee WHERE id = " + Integer.toString(id));
 
         try {
             if (rs.next()) {
-                return new Seller(
+                return new Employee(
                     id, rs.getString("first_name"), rs.getString("last_name"), 
                     rs.getString("phone_number"), rs.getString("email"), 
                     rs.getString("password"), rs.getDouble("max_loan"));
@@ -26,7 +25,7 @@ public class SellerData {
     }
 
     // Returns null on failure
-    public static Seller authenticate(String email, String password) {
+    public static Employee authenticate(String email, String password) {
         try {
             PreparedStatement statement = Database.getConnection().prepareStatement("SELECT id FROM dbo.Employee WHERE email = ? AND password = ?");
             statement.setString(1, email);
