09e0d245ffbf2ec2c7ea9de25d10e34db9aa1cc0
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-03 14:39:10 +0200

    Added SellerData.authenticate()

--- Ferrari/src/main/java/dk/eamv/ferrari/database/SellerData.java
+++ Ferrari/src/main/java/dk/eamv/ferrari/database/SellerData.java
@@ -3,6 +3,7 @@ package dk.eamv.ferrari.database;
 import dk.eamv.ferrari.database.Database;
 import dk.eamv.ferrari.seller.Seller;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -16,6 +17,24 @@ public class SellerData {
                     id, rs.getString("first_name"), rs.getString("last_name"), 
                     rs.getString("phone_number"), rs.getString("email"), 
                     rs.getString("password"), rs.getDouble("max_loan"));
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // Returns null on failure
+    public static Seller authenticate(String email, String password) {
+        try {
+            PreparedStatement statement = Database.getConnection().prepareStatement("SELECT id FROM dbo.Employee WHERE email = ? AND password = ?");
+            statement.setString(1, email);
+            statement.setString(1, password);
+
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return getSellerFromID(rs.getInt("id"));
             }
         } catch (SQLException exception) {
             exception.printStackTrace();
