92338ac11970628d7b29dc746a89ccc7fb58a766
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-03 09:52:47 +0200

    Delete ConnectionString.java

--- Ferrari/src/main/java/dk/eamv/ferrari/ConnectionString.java
+++
@@ -1,9 +0,0 @@
-package dk.eamv.ferrari;
-
-public class ConnectionString {
-    private static String connectionString = "";
-
-    public static String getConnectionString() {
-        return connectionString;
-    }
-}
