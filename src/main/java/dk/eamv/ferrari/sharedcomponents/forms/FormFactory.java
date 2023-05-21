c75ba816675e050e0efeb1bc1adf41457ab21714
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-21 23:09:56 +0200

    Removed unused static dialog from FormFactory

--- src/main/java/dk/eamv/ferrari/sharedcomponents/forms/FormFactory.java
+++ src/main/java/dk/eamv/ferrari/sharedcomponents/forms/FormFactory.java
@@ -6,43 +6,42 @@ import javafx.scene.control.Dialog;
 
 public final class FormFactory {
     /*
-     * Lavet af: Christian & Stefan
+     * Lavet af: Christian, Stefan og Benjamin
      */
 
     private static Form.Builder builder = new Form.Builder();
-    private static Dialog<?> dialog;
 
     public static void createCustomerFormDialogBox() {
         FormWrapper.wrapCreate(builder.buildCustomerForm(), CRUDType.CUSTOMER);
-        dialog = FormWrapper.getDialog();
+        Dialog<?> dialog = FormWrapper.getDialog();
         dialog.setTitle("Opret kunde");
         dialog.show();
     }
 
     public static void createCarFormDialogBox() {
         FormWrapper.wrapCreate(builder.buildCarForm(), CRUDType.CAR);
-        dialog = FormWrapper.getDialog();
+        Dialog<?> dialog = FormWrapper.getDialog();
         dialog.setTitle("Opret bil");
         dialog.show();
     }
 
     public static void createLoanFormDialogBox() {
         FormWrapper.wrapCreate(builder.buildLoanForm(), CRUDType.LOAN);
-        dialog = FormWrapper.getDialog();
+        Dialog<?> dialog = FormWrapper.getDialog();
         dialog.setTitle("Opret lån");
         dialog.show();
     }
 
     public static void updateCarFormDialogBox(Car car) {
         FormWrapper.wrapUpdate(builder.buildCarForm(), car);
-        dialog = FormWrapper.getDialog();
+        Dialog<?> dialog = FormWrapper.getDialog();
         dialog.setTitle("Opdater bil");
         dialog.show();
     }
 
     public static void updateCustomerFormDialogBox(Customer customer) {
         FormWrapper.wrapUpdate(builder.buildCustomerForm(), customer);
-        dialog = FormWrapper.getDialog();
+        Dialog<?> dialog = FormWrapper.getDialog();
         dialog.setTitle("Opdater kunde");
         dialog.show();
     }
