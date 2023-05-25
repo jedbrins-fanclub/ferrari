2331787bb3a05db4f3fdece9276ba01370eade66
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-25 13:17:43 +0200

    Added default user logged in for debugging

--- src/main/java/dk/eamv/ferrari/sessionmanager/SessionManager.java
+++ src/main/java/dk/eamv/ferrari/sessionmanager/SessionManager.java
@@ -1,6 +1,7 @@
 package dk.eamv.ferrari.sessionmanager;
 
 import dk.eamv.ferrari.scenes.employee.Employee;
+import dk.eamv.ferrari.scenes.employee.EmployeeModel;
 
 public class SessionManager {
     private static Employee user;
@@ -10,6 +11,10 @@ public class SessionManager {
     }
 
     public static Employee getUser() {
+        // DEBUGGING
+        if (user == null) {
+            user = EmployeeModel.read(1);
+        }
         return user;
     }
 }
