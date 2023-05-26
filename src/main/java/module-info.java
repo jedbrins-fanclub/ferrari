707125c6903df5e1f3a9f6745d40719c71367634
StilosGH <workmailchristian@gmail.com>
2023-05-26 20:06:48 +0200

    We done goofed - Readded module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module dk.eamv.ferrari {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    requires org.controlsfx.controls;
+    requires org.simplejavamail;
+    requires org.simplejavamail.core;
+
+    opens dk.eamv.ferrari to javafx.fxml;
+    exports dk.eamv.ferrari;
+}
