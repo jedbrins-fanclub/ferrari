7101520633cacf59da1ef447271ec95c2cfea83f
Benjamin Stigsen <benstigsen@posteo.net>
2023-05-30 11:54:29 +0200

    Reset Main.java

--- src/main/java/dk/eamv/ferrari/Main.java
+++ src/main/java/dk/eamv/ferrari/Main.java
@@ -1,11 +1,9 @@
 package dk.eamv.ferrari;
 
-import dk.eamv.ferrari.scenes.car.CarView;
-import dk.eamv.ferrari.scenes.customer.CustomerView;
+import dk.eamv.ferrari.scenes.loan.LoanView;
 import dk.eamv.ferrari.scenes.login.LoginView;
 import dk.eamv.ferrari.database.Database;
 import dk.eamv.ferrari.scenemanager.SceneManager;
-import dk.eamv.ferrari.sharedcomponents.email.EmailService;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -13,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         Database.init();
-        SceneManager.init(stage, CarView.getScene());
+        SceneManager.init(stage);
+        SceneManager.changeScene(LoanView.getScene());
     }
 
     public static void main(String[] args) {
