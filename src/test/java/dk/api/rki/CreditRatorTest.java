3b44f6c66ad364977142b7a5559c109a2b1bbe4c
Benjamin Stigsen <benstigsen@posteo.net>
2023-06-03 04:01:45 +0200

    Significantly sped up CreditRator testing

--- src/test/java/dk/api/rki/CreditRatorTest.java
+++ src/test/java/dk/api/rki/CreditRatorTest.java
@@ -7,15 +7,21 @@ import org.junit.jupiter.api.Test;
 // Made by: Benjamin
 public class CreditRatorTest {
     @Test
-    public void testCreditRating() {
-        Rating a = CreditRator.i().rate("1234560003");
-        Rating b = CreditRator.i().rate("1234560004");
-        Rating c = CreditRator.i().rate("1234560005");
-        Rating d = CreditRator.i().rate("1234560006");
+    public void testCreditRatingA() throws InterruptedException {
+        Thread a = runInThread("1234560003", Rating.A);
+        Thread b = runInThread("1234560004", Rating.B);
+        Thread c = runInThread("1234560005", Rating.C);
+        Thread d = runInThread("1234560006", Rating.D);
 
-        assertEquals(a, Rating.A);
-        assertEquals(b, Rating.B);
-        assertEquals(c, Rating.C);
-        assertEquals(d, Rating.D);
+        a.start(); b.start(); c.start(); d.start();
+        a.join(); b.join(); c.join(); d.join();
+    }
+
+    public Thread runInThread(String cpr, Rating expected) {
+        Thread thread = new Thread(() -> {
+            Rating rating = CreditRator.i().rate(cpr);
+            assertEquals(rating, expected);
+        });
+        return thread;
     }
 }
