1cbb34ff5d3bd0ba5f279927e3a07aadeb3193f8
StilosGH <workmailchristian@gmail.com>
2023-06-14 22:51:36 +0200

    Added tests for downpayment to loansize ratio

--- src/test/java/dk/eamv/ferrari/sharedcomponents/forms/CalculateInterestRateTest.java
+++ src/test/java/dk/eamv/ferrari/sharedcomponents/forms/CalculateInterestRateTest.java
@@ -12,15 +12,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dk.api.rki.Rating;
+import dk.eamv.ferrari.scenes.car.Car;
 import javafx.application.Platform;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class CalculateInterestRateTest {
+    private static Car car = mock(Car.class);
+    
     @BeforeAll
     public static void initOnce() {
         Platform.startup(() -> {});
         mockStatic(FormInputHandler.class);
+        when(car.getPrice()).thenReturn(1000000.0);
     }
     
     @BeforeEach
@@ -31,47 +35,66 @@ public class CalculateInterestRateTest {
         DatePicker mockDatePicker = new DatePicker(LocalDate.of(1, 1, 1));
         when(FormInputHandler.getDatePicker("Start dato DD/MM/ÅÅÅÅ")).thenReturn(mockDatePicker);
         when(FormInputHandler.getDatePicker("Slut dato DD/MM/ÅÅÅÅ")).thenReturn(mockDatePicker);
+        FormBinder.setCustomersCreditScore(Rating.D);
     }
     
     //Credit score
     @Test
     public void creditscoreC_ShouldAdd_3() {
         FormBinder.setCustomersCreditScore(Rating.C);
+
         assertThat(FormBinder.calculateInterestRate()).isEqualTo(3);
     }
 
     @Test
     public void creditscoreB_ShouldAdd_2() {
         FormBinder.setCustomersCreditScore(Rating.B);
+
         assertThat(FormBinder.calculateInterestRate()).isEqualTo(2);
     }
     
     @Test
     public void creditscoreA_ShouldAdd_1() {
         FormBinder.setCustomersCreditScore(Rating.A);
+
         assertThat(FormBinder.calculateInterestRate()).isEqualTo(1);
     }
 
     //Banks rate
     @Test
     public void verifyBanksInterestRate() {
+        double mockInterestRate = 8.5;
+        FormBinder.setBanksInterestRate(mockInterestRate);
 
+        assertThat(FormBinder.calculateInterestRate()).isEqualTo(mockInterestRate);
     }
 
     //Loansize - downpayment
     @Test
     public void lessThanHalf_ShouldAdd_1() {
+        when(FormInputHandler.getTextField("Udbetaling")).thenReturn(new TextField("499999.0"));
+        when(FormInputHandler.getDouble("Udbetaling")).thenReturn(499999.0);
+        when(FormInputHandler.getEntityFromComboBox("Bil")).thenReturn(car);
 
+        assertThat(FormBinder.calculateInterestRate()).isEqualTo(1);
     }
 
     @Test
     public void half_ShouldAdd_0() {
+        when(FormInputHandler.getTextField("Udbetaling")).thenReturn(new TextField("500000.0"));
+        when(FormInputHandler.getDouble("Udbetaling")).thenReturn(500000.0);
+        when(FormInputHandler.getEntityFromComboBox("Bil")).thenReturn(car);
 
+        assertThat(FormBinder.calculateInterestRate()).isEqualTo(0);
     }
-
+     
     @Test
     public void moreThanHalf_ShouldAdd_0() {
+        when(FormInputHandler.getTextField("Udbetaling")).thenReturn(new TextField("500001.0"));
+        when(FormInputHandler.getDouble("Udbetaling")).thenReturn(500001.0);
+        when(FormInputHandler.getEntityFromComboBox("Bil")).thenReturn(car);
 
+        assertThat(FormBinder.calculateInterestRate()).isEqualTo(0);
     }
 
     //Dates
