00b250b8bca0d1c1373cad2482b452db0e8fc181
Benjamin Stigsen <benstigsen@posteo.net>
2023-06-15 11:38:56 +0200

    Added ENTER keypress support on LoginView

--- src/main/java/dk/eamv/ferrari/scenes/login/LoginController.java
+++ src/main/java/dk/eamv/ferrari/scenes/login/LoginController.java
@@ -4,12 +4,14 @@ import dk.eamv.ferrari.scenes.employee.Employee;
 import dk.eamv.ferrari.scenes.frontpage.FrontpageView;
 import dk.eamv.ferrari.managers.SceneManager;
 import dk.eamv.ferrari.managers.SessionManager;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 // Made by: Christian
 // Checked by: Benjamin
 // Modified by: Benjamin
 public class LoginController {
-    public static void authenticate() {
+    protected static void authenticate() {
         Employee employee = LoginModel.authenticate(LoginView.getUsernameInput(), LoginView.getPasswordInput());
         if (employee != null) {
             SessionManager.setUser(employee);
@@ -17,6 +19,12 @@ public class LoginController {
             LoginView.getErrorLabel().setVisible(false);
         } else {
             LoginView.getErrorLabel().setVisible(true);
+        }
+    }
+
+    protected static void keypress(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            authenticate();
         }
     }
 }
