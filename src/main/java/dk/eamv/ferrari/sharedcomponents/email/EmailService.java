0a70a2d845e27e205101441f0c00ebbca2b585b9
Benjamin Stigsen <benstigsen@posteo.net>
2023-06-17 16:13:15 +0200

    Added Employee name to email being sent

--- src/main/java/dk/eamv/ferrari/sharedcomponents/email/EmailService.java
+++ src/main/java/dk/eamv/ferrari/sharedcomponents/email/EmailService.java
@@ -5,6 +5,9 @@ import org.simplejavamail.api.mailer.config.TransportStrategy;
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.MailerBuilder;
 
+import dk.eamv.ferrari.managers.SessionManager;
+import dk.eamv.ferrari.scenes.employee.Employee;
+
 // Made by: Mikkel
 
 /**
@@ -16,26 +19,27 @@ public class EmailService {
      * Standard method for sending an email from seller to sales manager.
      */
     public static void sendEmail() {
+        Employee employee = SessionManager.getUser();
         Email email = EmailBuilder.startingBlank()
-                .from("Sælgeren", "ferrari.herning.seller@gmail.com")
-                .to("Salgschefen", "ferrari.herning.salesman@gmail.com")
-                .withSubject("Lån overskrider min grænse")
-                .withPlainText("""
-                        Hej salgschef
-                        
-                        Jeg har oprettet et lånetilbud som overskrider min tilladte beløbsgrænse.
-                        Vil du godkende aftalen?
-                        
-                        Mvh
-                        Sælger
-                        """)
-                .buildEmail();
+            .from("Sælgeren", "ferrari.herning.seller@gmail.com")
+            .to("Salgschefen", "ferrari.herning.salesman@gmail.com")
+            .withSubject("Lån overskrider min grænse")
+            .withPlainText(String.format("""
+                Hej salgschef
+
+                Jeg har oprettet et lånetilbud som overskrider min tilladte beløbsgrænse.
+                Vil du godkende aftalen?
+
+                Mvh
+                %s %s
+                """, employee.getFirstName(), employee.getLastName()))
+            .buildEmail();
 
         // Sends the email on a new thread to keep the system responsive.
         new Thread(() -> MailerBuilder
-                .withSMTPServer("Smtp.gmail.com", 587, "ferrari.herning.seller@gmail.com", "jeetyoldfpfisysb")
-                .withTransportStrategy(TransportStrategy.SMTP_TLS)
-                .buildMailer()
-                .sendMail(email)).start();
+            .withSMTPServer("Smtp.gmail.com", 587, "ferrari.herning.seller@gmail.com", "jeetyoldfpfisysb")
+            .withTransportStrategy(TransportStrategy.SMTP_TLS)
+            .buildMailer()
+            .sendMail(email)).start();
     }
 }
